a745aa00aacff03246a9430eef7884994d6aacaa
Marika <mbomjour@gmail.com>
2023-03-07 17:58:40 +0300

    Volume added and tested

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -21,12 +21,38 @@ public class Radio {
     public void next() {
         if (currentStation == 9) {
             setCurrentStation(0);
-        } else setCurrentStation(currentStation + 1);
+        } else currentStation++;
     }
 
     public void prev() {
         if (currentStation == 0) {
             setCurrentStation(9);
-        } else setCurrentStation(currentStation - 1);
+        } else currentStation--;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == 100) {
+            setCurrentVolume(100);
+        } else currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == 0) {
+            setCurrentVolume(0);
+        } else currentVolume--;
     }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -10,9 +10,9 @@ public class RadioTest {
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/stations.csv")
 
-    public void shouldSetStation(int setStation, int expected) { // номер станции
+    public void shouldSetStation(int target, int expected) { // номер станции
         Radio radio = new Radio();
-        radio.setCurrentStation(setStation);
+        radio.setCurrentStation(target);
 
         int actual = radio.getCurrentStation();
 
@@ -63,6 +63,66 @@ public class RadioTest {
 
         int expected = 9;
         int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/volume.csv")
+
+    public void shouldSetVolume(int target, int expected) { // номер станции
+        Radio radio = new Radio();
+        radio.setCurrentVolume(target);
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolume() { // номер станции больше допустимого значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(72);
+        radio.increaseVolume();
+
+        int expected = 73;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotIncreaseVolumeOverMax() { // номер станции больше допустимого значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolume() { // номер станции больше допустимого значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(38);
+        radio.decreaseVolume();
+
+        int expected = 37;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotDecreaseVolumeUnderMin() { // номер станции больше допустимого значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
 
         Assertions.assertEquals(expected, actual);
     }
